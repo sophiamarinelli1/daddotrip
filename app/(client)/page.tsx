e545947aa394655d62a7c4cfb4ddd9ba8d07a069
Sophia Marinelli <phiamarinelli@gmail.com>
2024-08-24 23:18:44 -0400

    remove export

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 import { Post } from "../utils/interface";
 import PostComponent from "../components/PostComponent";
 
-export async function getPosts() {
+async function getPosts() {
 	const query = `*[_type == "post"] {
   title,
   slug,
